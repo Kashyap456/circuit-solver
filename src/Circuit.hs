0b3c0c8b5175bcfd3ef6ee00851a35e1f092237f
Brady Zhou <bradyzhou314159@gmail.com>
2024-12-05 23:36:53 -0500

    Finish circuit parsing

--- src/Circuit.hs
+++ src/Circuit.hs
@@ -18,7 +18,10 @@ module Circuit
 where
 
 import Data.Map
-import Prelude
+import Data.List qualified as List
+import Data.Text (strip, pack, unpack)
+import Prelude hiding (lookup)
+import YamlParser (parseYAMLFile, YAMLValue (..), extractList, extractMap, extractString)
 
 newtype NodeID = NodeID String deriving (Show, Eq, Ord)
 
@@ -70,5 +73,79 @@ validate :: Circuit -> Maybe Circuit
 validate = undefined
 
 -- Create circuit from a YAML file
-parseCircuit :: String -> Maybe Circuit
-parseCircuit = undefined
+parseCircuit :: String -> IO (Maybe Circuit)
+parseCircuit filename = do
+  parsedYAML <- parseYAMLFile filename
+  case parsedYAML of
+    Right (YAMLMap m) -> return (getCircuit m)
+    _ -> return Nothing
+
+saveCircuit :: String -> Circuit -> IO ()
+saveCircuit = undefined
+
+-- Helper for getting a Var type from a map
+getVar :: Map String YAMLValue -> String -> Maybe Var
+getVar m paramName = case lookup paramName m of
+  Just (YAMLString s) -> Just (Unknown (Parameter (ComponentID s)))
+  Just (YAMLDouble d) -> Just (Known d)
+  _ -> Nothing
+
+-- Given a map of the component
+getComponentType :: Map String YAMLValue -> Maybe ComponentType
+getComponentType m = do
+  id <- lookup "id" m >>= extractString
+  t <- lookup "type" m >>= extractString
+  (case t of
+    "voltage" -> f "voltage" VSource
+    "resistor" -> f "resistance" Resistor
+    _ -> Nothing) where
+      f paramName constructor = do
+        var <- getVar m paramName
+        pure (constructor var)
+
+-- Gets a single node
+getNode :: Map String YAMLValue -> Maybe Node
+getNode m = do
+  id <- lookup "id" m >>= extractString
+  voltage <- getVar m "voltage"
+  pure (Node (NodeID id) voltage)
+
+getNodes :: Map String YAMLValue -> Maybe (Map NodeID Node)
+getNodes m =
+  if notMember "nodes" m then Nothing
+  else do
+    l <- lookup "nodes" m >>= extractList
+    return (fromList (List.foldr f [] l)) where
+      f x acc = case extractMap x of
+        Just m -> case getNode m of
+          Just n -> (nodeID n, n) : acc
+          _ -> acc
+        Nothing -> acc
+
+-- Gets a single component
+getComponent :: Map String YAMLValue -> Maybe Component
+getComponent m = do
+  id <- lookup "id" m >>= extractString
+  current <- getVar m "current"
+  pos <- lookup "pos" m >>= extractString
+  neg <- lookup "neg" m >>= extractString
+  t <- getComponentType m
+  pure (Component (ComponentID id) t current (NodeID pos) (NodeID neg))
+
+getComponents :: Map String YAMLValue -> Maybe [Component]
+getComponents m =
+  if notMember "components" m then Nothing
+  else do
+    l <- lookup "components" m >>= extractList
+    return (List.foldr f [] l) where
+      f x acc = case extractMap x of
+        Just m -> case getComponent m of
+          Just c -> c : acc
+          _ -> acc
+        Nothing -> acc
+
+getCircuit :: Map String YAMLValue -> Maybe Circuit
+getCircuit m = do
+  nodes <- getNodes m
+  components <- getComponents m
+  pure (Circuit nodes components)
