73694cb4fd28c1161d2444f1cb94760f5de9964e
Kashyap Chaturvedula <kashyapc@seas.upenn.edu>
2024-12-07 12:15:12 -0500

    handle type change between my branch and main (components as a Map instead of List)

--- src/Circuit.hs
+++ src/Circuit.hs
@@ -20,15 +20,15 @@ module Circuit
   )
 where
 
-import Data.Map
-import Data.List qualified as List
-import Data.Text (strip, pack, unpack)
-import Prelude hiding (lookup)
-import YamlParser (parseYAMLFile, YAMLValue (..), extractList, extractMap, extractString)
 import Control.Monad (when)
+import Data.List qualified as List
+import Data.Map
+import Data.Text (pack, strip, unpack)
+import GHC.IO.IOMode (IOMode (WriteMode))
 import System.FilePath (takeExtension)
-import GHC.IO.IOMode (IOMode(WriteMode))
 import System.IO
+import YamlParser (YAMLValue (..), extractList, extractMap, extractString, parseYAMLFile)
+import Prelude hiding (lookup)
 
 newtype NodeID = NodeID String deriving (Show, Eq, Ord)
 
@@ -89,15 +89,16 @@ parseCircuit filename = do
 
 -- Convert a variable to a string
 varToString :: Var -> String
-varToString v = case v of 
-    Known d -> show d
-    Unknown (NodeVoltage (NodeID v)) -> v
-    Unknown (Parameter (ComponentID c)) -> c
+varToString v = case v of
+  Known d -> show d
+  Unknown (NodeVoltage (NodeID v)) -> v
+  Unknown (Parameter (ComponentID c)) -> c
 
 -- Convert a circuit's nodes to its YAML string
 nodesToString :: Map NodeID Node -> String
-nodesToString nodeMap = "nodes:\n" ++ foldrWithKey f "" nodeMap where
-  f (NodeID key) node acc = 
+nodesToString nodeMap = "nodes:\n" ++ foldrWithKey f "" nodeMap
+  where
+    f (NodeID key) node acc =
       " - id: " ++ key ++ "\n   voltage: " ++ varToString (nodeVoltage node) ++ "\n" ++ acc
 
 componentTypeToString :: ComponentType -> String
@@ -106,24 +107,31 @@ componentTypeToString t = case t of
   Resistor resistance -> "   type: resistor\n   resistance: " ++ varToString resistance ++ "\n"
 
 -- Convert a circuit's nodes to its YAML string
-componentsToString :: [Component] -> String
-componentsToString components = "components:\n" ++ List.foldr f "" components where
-  f comp acc = 
-    let (ComponentID id) = componentID comp in
-      let (NodeID posID) = nodePos comp in 
-        let (NodeID negID) = nodeNeg comp in
-          " - id: " ++ id ++
-          "\n   circuit: " ++ varToString (current comp) ++
-          "\n   pos: " ++ posID ++
-          "\n   neg: " ++ negID ++
-          "\n" ++ componentTypeToString (componentType comp) ++ acc
+componentsToString :: Map ComponentID Component -> String
+componentsToString components = "components:\n" ++ foldrWithKey f "" components
+  where
+    f compId comp acc =
+      let (ComponentID id) = componentID comp
+       in let (NodeID posID) = nodePos comp
+           in let (NodeID negID) = nodeNeg comp
+               in " - id: "
+                    ++ id
+                    ++ "\n   circuit: "
+                    ++ varToString (current comp)
+                    ++ "\n   pos: "
+                    ++ posID
+                    ++ "\n   neg: "
+                    ++ negID
+                    ++ "\n"
+                    ++ componentTypeToString (componentType comp)
+                    ++ acc
 
 -- Convert a circuit to its YAML file
 circuitToString :: Circuit -> String
 circuitToString circuit =
-  let nodeMap = nodes circuit in
-    let comps = components circuit in
-      nodesToString nodeMap ++ componentsToString comps
+  let nodeMap = nodes circuit
+   in let comps = components circuit
+       in nodesToString nodeMap ++ componentsToString comps
 
 saveCircuit :: String -> Circuit -> IO ()
 saveCircuit filename circuit = do
@@ -131,7 +139,7 @@ saveCircuit filename circuit = do
   writeFile filename (circuitToString circuit)
   return ()
 
---- Circuit Modifiers --- 
+--- Circuit Modifiers ---
 
 -- Helper for getting a Var type from a map
 getVar :: Map String YAMLValue -> String -> Maybe Var
@@ -145,13 +153,15 @@ getComponentType :: Map String YAMLValue -> Maybe ComponentType
 getComponentType m = do
   id <- lookup "id" m >>= extractString
   t <- lookup "type" m >>= extractString
-  (case t of
-    "voltage" -> f "voltage" VSource
-    "resistor" -> f "resistance" Resistor
-    _ -> Nothing) where
-      f paramName constructor = do
-        var <- getVar m paramName
-        pure (constructor var)
+  ( case t of
+      "voltage" -> f "voltage" VSource
+      "resistor" -> f "resistance" Resistor
+      _ -> Nothing
+    )
+  where
+    f paramName constructor = do
+      var <- getVar m paramName
+      pure (constructor var)
 
 -- Gets a single node
 getNode :: Map String YAMLValue -> Maybe Node
@@ -162,15 +172,17 @@ getNode m = do
 
 getNodes :: Map String YAMLValue -> Maybe (Map NodeID Node)
 getNodes m =
-  if notMember "nodes" m then Nothing
-  else do
-    l <- lookup "nodes" m >>= extractList
-    return (fromList (List.foldr f [] l)) where
-      f x acc = case extractMap x of
-        Just m -> case getNode m of
-          Just n -> (nodeID n, n) : acc
-          _ -> acc
-        Nothing -> acc
+  if notMember "nodes" m
+    then Nothing
+    else do
+      l <- lookup "nodes" m >>= extractList
+      return (fromList (List.foldr f [] l))
+  where
+    f x acc = case extractMap x of
+      Just m -> case getNode m of
+        Just n -> (nodeID n, n) : acc
+        _ -> acc
+      Nothing -> acc
 
 -- Gets a single component
 getComponent :: Map String YAMLValue -> Maybe Component
@@ -182,17 +194,19 @@ getComponent m = do
   t <- getComponentType m
   pure (Component (ComponentID id) t current (NodeID pos) (NodeID neg))
 
-getComponents :: Map String YAMLValue -> Maybe [Component]
+getComponents :: Map String YAMLValue -> Maybe (Map ComponentID Component)
 getComponents m =
-  if notMember "components" m then Nothing
-  else do
-    l <- lookup "components" m >>= extractList
-    return (List.foldr f [] l) where
-      f x acc = case extractMap x of
-        Just m -> case getComponent m of
-          Just c -> c : acc
-          _ -> acc
-        Nothing -> acc
+  if notMember "components" m
+    then Nothing
+    else do
+      l <- lookup "components" m >>= extractList
+      return (fromList (List.foldr f [] l))
+  where
+    f x acc = case extractMap x of
+      Just m -> case getComponent m of
+        Just c -> (componentID c, c) : acc
+        _ -> acc
+      Nothing -> acc
 
 getCircuit :: Map String YAMLValue -> Maybe Circuit
 getCircuit m = do
@@ -200,8 +214,17 @@ getCircuit m = do
   components <- getComponents m
   pure (Circuit nodes components)
 
-
 c :: Circuit
-c = Circuit {nodes = fromList [(NodeID "n1",Node {nodeID = NodeID "n1", nodeVoltage = Unknown (Parameter (ComponentID "n1"))}),(NodeID "n2",Node {nodeID = NodeID "n2", nodeVoltage = Unknown (Parameter (ComponentID "n1"))})], components = [Component {componentID = ComponentID "r1", componentType = Resistor {resistance = Known 100.0}, current = Unknown (Parameter (ComponentID "i_r1")), nodePos = NodeID "n1", nodeNeg = NodeID "n2"},Component {componentID = ComponentID "v1", componentType = VSource {voltage = Known 5.0}, current = Unknown (Parameter (ComponentID "i_r1")), nodePos = NodeID "n2", nodeNeg = NodeID "n1"}]}
-
-
+c =
+  Circuit
+    { nodes =
+        fromList
+          [ (NodeID "n1", Node {nodeID = NodeID "n1", nodeVoltage = Unknown (Parameter (ComponentID "n1"))}),
+            (NodeID "n2", Node {nodeID = NodeID "n2", nodeVoltage = Unknown (Parameter (ComponentID "n1"))})
+          ],
+      components =
+        fromList
+          [ (ComponentID "r1", Component {componentID = ComponentID "r1", componentType = Resistor {resistance = Known 100.0}, current = Unknown (Parameter (ComponentID "i_r1")), nodePos = NodeID "n1", nodeNeg = NodeID "n2"}),
+            (ComponentID "v1", Component {componentID = ComponentID "v1", componentType = VSource {voltage = Known 5.0}, current = Unknown (Parameter (ComponentID "i_r1")), nodePos = NodeID "n2", nodeNeg = NodeID "n1"})
+          ]
+    }
