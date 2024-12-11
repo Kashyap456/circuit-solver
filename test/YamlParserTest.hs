2841b718c8184f0a02698388c50c2bab2cf25b24
Brady Zhou <bradyzhou314159@gmail.com>
2024-12-11 00:14:26 -0500

    Create generators for YAMLValues

--- test/YamlParserTest.hs
+++ test/YamlParserTest.hs
@@ -1,9 +1,12 @@
 module YamlParserTest where
 
-import YamlParser
+import YamlParser (YAMLValue(..), parseYAMLFile)
 import qualified Data.Map as Map
 import Test.HUnit
+import Test.QuickCheck (Gen, Arbitrary (arbitrary), elements, listOf)
+import Test.QuickCheck qualified as QC
 
+--- UNIT TESTCASES ---
 
 compareYAML :: String -> YAMLValue -> IO ()
 compareYAML filename expected = do
@@ -37,3 +40,56 @@ parseSimpleTest =
 
 parseEmptyTest :: Test
 parseEmptyTest = "Parse Empty" ~: compareYAML "test/sample_files/empty.yaml" YAMLNull
+
+
+--- GENERATORS ---
+genSafeChar :: Gen Char
+genSafeChar = elements ['a'..'z']
+
+genSafeString :: Gen String
+genSafeString = do
+    c <- genSafeChar
+    s <- listOf genSafeChar
+    return (c : s)
+
+genYAMLString :: Gen YAMLValue
+genYAMLString = YAMLString <$> genSafeString
+
+genYAMLDouble :: Gen YAMLValue
+genYAMLDouble = do
+    numerator <- QC.chooseInt (0, 10000)
+    denominator <- QC.chooseInt (1, 200)
+    return (YAMLDouble (fromIntegral numerator / fromIntegral denominator))
+
+genSmallList :: Gen a -> Gen [a]
+genSmallList g = do
+    fst <- g
+    rst <- QC.resize 3 (listOf g)
+    return (fst : rst)
+
+genYAMLList :: Gen YAMLValue
+genYAMLList = do
+    elts <- genSmallList genYAMLValue
+    return (YAMLList elts)
+
+genKeyValuePair :: Gen (String, YAMLValue)
+genKeyValuePair = do
+    key <- genSafeString
+    value <- genYAMLValue
+    return (key, value)
+
+
+genYAMLMap :: Gen YAMLValue
+genYAMLMap = do
+    elts <- genSmallList genKeyValuePair
+    return (YAMLMap (Map.fromList elts))
+
+
+genYAMLValue :: Gen YAMLValue
+genYAMLValue =
+    QC.frequency [
+        (4, genYAMLString),
+        (4, genYAMLDouble),
+        (2, genYAMLList),
+        (2, genYAMLMap)
+    ]
